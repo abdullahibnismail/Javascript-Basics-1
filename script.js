6e3103d789033ac008e840fe6d0c5cf189e1aa26
ahmed abdullah <Ahmed Abdullah>
2020-12-28 00:18:58 -0800

    added program 3

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-// Program 2
-var height = parseInt( prompt("Enter Height in Inches") );
-console.log("height in centimeters =", 2.54 * height);
+// Program 3
+var firstSubject = parseInt( prompt("Enter Marks of First Subject") );
+var secondSubject = parseInt( prompt("Enter Marks of Second Subject") );
+var thirdSubject = parseInt( prompt("Enter Marks of third Subject") );
+var average = firstSubject + secondSubject + thirdSubject / 3 ;
+if( average>80 ){
+    console.log( "you are above standard, Admission Granted!");
+}
